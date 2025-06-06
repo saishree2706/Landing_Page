776cbe3894226e5cb109970da88ffd8fa1a8c98a
Saishree <56631066+saishree2706@users.noreply.github.com>
2025-06-06 17:27:44 +0000

    update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,12 @@ mongoose.connect(`${MONGO_URI}`, {
 .then(() => console.log('✅ Connected to MongoDB'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
 
-app.use(cors());
+app.use(cors({
+  origin: [""],
+        methods: ["POST", "GET"],
+        credentials: true
+
+}));
 app.use(express.json());
 
 app.use('/send-email', emailRoutes); // ✅ This registers your POST /send-email routes')
